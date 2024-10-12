c79c30bda5125b94e489aa5a7444fc7ff61b7182
Patrik Mintěl <ja@patrick115.eu>
2024-10-12 17:59:26 +0200

    Setup Neovim

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        --config = function()
+        --    require("mason-lspconfig").setup({
+        --        ensure_installed = { "lua_ls", "clangd", "svelte", "vuels", "ts_ls", "bashls", "cssls", "cmake", "html" },
+        --    })
+        --end,
+        lazy = false,
+        opts = {
+            auto_install = true
+        }
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
