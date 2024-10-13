ced5cbb20bb0a469ccb313fee7c0941416e0b834
Patrik Mintěl <ja@patrick115.eu>
2024-10-13 23:12:09 +0200

    Toggle terminal

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,3 +5,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+
+vim.api.nvim_set_keymap(  't'  ,  '<Leader><ESC>'  ,  '<C-\\><C-n>'  ,  {noremap = true}  )
+vim.keymap.set("n", "<Leader>t", ":ToggleTerm<CR>")
