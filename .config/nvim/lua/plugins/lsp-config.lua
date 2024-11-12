70dc45f7c168fcf9ddba032e3e9bbc4defe5abc8
Patrik Mintěl <ja@patrick115.eu>
2024-11-12 18:11:26 +0100

    Reformat

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,65 +1,71 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        --config = function()
-        --    require("mason-lspconfig").setup({
-        --        ensure_installed = { "lua_ls", "clangd", "svelte", "vuels", "ts_ls", "bashls", "cssls", "cmake", "html" },
-        --    })
-        --end,
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            local masonRegistry = require("mason-registry")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		--config = function()
+		--    require("mason-lspconfig").setup({
+		--        ensure_installed = { "lua_ls", "clangd", "svelte", "vuels", "ts_ls", "bashls", "cssls", "cmake", "html" },
+		--    })
+		--end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local masonRegistry = require("mason-registry")
 
-            local vuels = masonRegistry.get_package("vue-language-server"):get_install_path()
-                .. "/node_modules/@vue/language-server"
+			local vuels = masonRegistry.get_package("vue-language-server"):get_install_path()
+				.. "/node_modules/@vue/language-server"
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-                init_options = {
-                    plugins = {
-                        {
-                            name = "@vue/typescript-plugin",
-                            location = vuels,
-                            languages = { "vue" },
-                        },
-                    },
-                },
-                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.svelte.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.volar.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vuels,
+							languages = { "vue" },
+						},
+					},
+				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.volar.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.glslls.setup({
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
+		end,
+	},
 }
