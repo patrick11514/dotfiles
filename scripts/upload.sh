148c4ed2d535ade1fe38bb353d157c8a682b5323
Patrik Mintěl <ja@patrick115.eu>
2025-03-28 11:45:25 +0100

    Allow hypridle, because bug with monitors was fixed + make notification about upload better

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,4 +4,4 @@ chmod 644 $FILENAME
 RANDOMNAME=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 10)
 scp $FILENAME images:/var/www/patrick115.eu/upload/.storage/$RANDOMNAME.png
 echo "https://upload.patrick115.eu/screenshot/$RANDOMNAME.png" | wl-copy
-notify-send -t 5000 -u low "Screenshot has been uploaded"
+notify-send -t 5000 -u low -i /tmp/screenshot.png "Image uploader" "Screenshot has been uploaded"
