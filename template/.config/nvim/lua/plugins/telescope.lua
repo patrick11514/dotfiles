b0b7cb8c90c1e6e1053e18766ee790cf003ccf95
Patrik Mintěl <ja@patrick115.eu>
2025-07-04 18:26:55 +0200

    Don't search build/.git/dist folders

--- template/.config/nvim/lua/plugins/telescope.lua
+++ template/.config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,11 @@ return {
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules", ".git", "build", "dist },
+				},
+			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader><C-p>", builtin.live_grep, {})
