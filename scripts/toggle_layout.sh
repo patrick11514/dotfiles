77585564742a73e9059a8d49ec709716125fb99b
patrick11514 <ja@patrick115.eu>
2025-12-08 17:47:25 +0100

    fix(scripts): Toggle layout script should move windows

--- scripts/toggle_layout.sh
+++ scripts/toggle_layout.sh
@@ -7,6 +7,10 @@ if [ -f /tmp/.home_layout.lock ]; then
     ln -sf ~/.config/hypr/rules/single.conf ~/.config/hypr/rules.conf
     hyprctl reload
 
+    hyprctl --batch "$(
+        for i in {1..10}; do printf "dispatch moveworkspacetomonitor %s eDP-1;" $i; done
+    )"
+
     hyprctl dispatch movetoworkspace 5,class\:\(discord\)
     hyprctl dispatch movetoworkspace 4,class\:\(Slack\)
 
@@ -17,6 +21,10 @@ else
     ln -sf ~/.config/hypr/monitors/home.conf ~/.config/hypr/monitors.conf
     ln -sf ~/.config/hypr/rules/home.conf ~/.config/hypr/rules.conf
     hyprctl reload
+
+    hyprctl --batch "$(
+        for i in {1..10}; do printf "dispatch moveworkspacetomonitor %s HDMI-A-1;" $i; done
+    )"
 
     hyprctl dispatch movetoworkspace 9,class\:\(discord\)
     hyprctl dispatch movetoworkspace 10,class\:\(Slack\)
