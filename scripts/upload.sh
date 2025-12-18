47ae401f4a0312652615d3577cbcbd1979b88c1e
Patrik Mintěl <ja@patrick115.eu>
2025-12-18 19:25:41 +0100

    Update upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+cd $(dirname "$0")
+
+#include API_KEY
+source api_key
+
 FILENAME="/tmp/screenshot.png"
 chmod 644 $FILENAME
 RANDOMNAME=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 10)
-scp $FILENAME images:/var/www/patrick115.eu/upload/.storage/$RANDOMNAME.png
-echo "https://upload.patrick115.eu/screenshot/$RANDOMNAME.png" | wl-copy
-notify-send -t 5000 -u low -i /tmp/screenshot.png "Image uploader" "Screenshot has been uploaded"
+
+RESULT=$(curl -s -H "Authorization: Bearer $API_KEY" -F "file=@/tmp/screenshot.png" https://upload.patrick115.eu/api/upload)
+SUCCESS=$( echo "$RESULT" | jq -r '.status')
+
+if [[ $SUCCESS == "true" ]]; then
+    URL=$(echo "$RESULT" | jq -r '.data')
+    echo $URL | wl-copy
+    notify-send -t 5000 -u low -i /tmp/screenshot.png "Image uploader" "Screenshot has been uploaded"
+else
+    notify-send -t 5000 -u critical -i /tmp/screenshot.png "Image uploader" "Screenshot upload failed"
+    exit 1
+fi
